94a0b4f0cceae734929e8297d723f29777177372
Harry06-X <harry-hounsou@epitech.eu>
2025-09-15 11:17:14 +0100

    Update og the middleware authentification

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,21 @@ const auth = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ msg: "No token, authorization denied" });
+        return res.status(401).json({ msg: "No token, authorization denied"});
     }
 
     const token = authHeader.split(" ")[1];
 
+    if (!token) {
+        return res.status(401).json({msg : "Accesse denied, invalide token"});
+    }
+    
     try {
         const decoded = jwt.verify(token, process.env.SECRET);
         req.user = decoded;
         next();
     } catch (err) {
-        return res.status(403).json({ msg: "Token is not valid" });
+        return res.status(403).json({ msg: "Token is not valid"});
     }
 };
 auth();
