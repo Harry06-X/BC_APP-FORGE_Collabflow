e0b6ef3c18cce847e8eae3bf44e887db52b89acd
Harry06-X <harry-hounsou@epitech.eu>
2025-09-16 18:34:26 +0100

    Setting up the project routes for post, get, put and delete request

---
+++ routes/projectRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const {addProject, listProjects, updateProject, deleteProject} = require("../controllers/projectController");
+
+router.post("/", addProject);
+router.get("/", listProjects);
+router.put("/:id", updateProject);
+router.delete("/:id", deleteProject);
+
+module.exports = router;
