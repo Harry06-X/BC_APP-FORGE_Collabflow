dbc37dab2be2c340b358290c49fe94698aa120a9
Harry06-X <harry-hounsou@epitech.eu>
2025-09-16 18:33:26 +0100

    Adding the task routs and the project route

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const connectDB = require('./config/db');
+
 dotenv.config();
+const app = express();
 const port = process.env.PORT || 5000;
+const taskRoutes = require("./routes/taskRoutes");
+const projectRoutes = require("./routes/projectRoutes");
 
 app.use(express.json());
 
+connectDB();
+
+app.use("/api/tasks", taskRoutes);
+app.use("/api/projects", projectRoutes);
+
 app.get("/", (req, res) => {
-    res.send("Welcome to CollabFlow API connected to MongoDB Atlas!");
+    res.send("Welcome to CollabFlow API connected to MongoDB Atlas!!!");
 });
 
-connectDB().then(() => {
-    app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-    });
 });
