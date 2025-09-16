87435489d01f8044d67bce16eafc4a7c552934a0
Harry06-X <harry-hounsou@epitech.eu>
2025-09-16 11:04:29 +0100

    Connection to database using Atlas database and a new port

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const port = 27017;
+const connectDB = require('./config/db');
+dotenv.config();
+const port = 5000;
 
 app.get("/", (req, res) => {
-    console.log("Helooo ");
-    res.send("Welcome to the database");
+    res.send("Welcome to the database!");
 });
 
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+    connectDB();
+    console.log(`Example app listening at http://localhost:${port}`);
 });
