9ee566a7232d9efbd4fc947eecb04b5c2fcf45e1
Harry06-X <harry-hounsou@epitech.eu>
2025-09-16 18:34:55 +0100

    Setting up the task routes for post, get, put and delete request

---
+++ routes/taskRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const {addTask, listTasks, updateTask, deleteTask} = require("../controllers/taskController");
+
+router.post("/", addTask);
+router.get("/", listTasks);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+module.exports = router;
