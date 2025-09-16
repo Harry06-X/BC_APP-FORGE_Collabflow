42093dbda912b92d910a7bed121c1e95d92ab43f
Harry06-X <harry-hounsou@epitech.eu>
2025-09-16 16:10:19 +0100

    Correction from Atlas connection before database connection to database conection before Atlas connection

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const app = express();
 const dotenv = require("dotenv");
 const connectDB = require('./config/db');
 dotenv.config();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the database!");
+    res.send("Welcome to CollabFlow API connected to MongoDB Atlas!");
 });
 
-app.listen(port, () => {
-    connectDB();
-    console.log(`Example app listening at http://localhost:${port}`);
+connectDB().then(() => {
+    app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+    });
 });
