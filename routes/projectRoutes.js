e2c522ccf6a3748d57906fd3a8d7b2b3bea5adf6
Harry06-X <harry-hounsou@epitech.eu>
2025-09-18 17:39:49 +0100

    Adaptation of the routes in order to list tasks under each project

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { addProject, listProjects, updateProject, deleteProject } = require("../controllers/projectController");
+const { addProject, listProjects, getProjectWithTasks, updateProject, deleteProject } = require("../controllers/projectController");
+//const { get } = require("mongoose");
+const { addTask, listTasksByProject } = require("../controllers/taskController");
 
 router.post("/", addProject);
 router.get("/", listProjects);
+router.put("/:id", getProjectWithTasks);
 router.put("/:id", updateProject);
 router.delete("/:id", deleteProject);
+
+router.post("/:projectId/tasks", addTask);
+router.get("/:projectId/tasks", listTasksByProject)
 
 module.exports = router;
