b1853da428ba89be8d1693516c3498906746b2eb
Harry06-X <harry-hounsou@epitech.eu>
2025-09-22 11:47:38 +0100

    Initilization of the salt variable before using

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ["admin", "member"],
-        default: "memmber"
+        default: "member"
     },
 
     createAt: {
@@ -31,10 +31,15 @@ const userSchema = new mongoose.Schema({
 });//, { timestamps: true });
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password"))
-        return next();
-    const salt = await bcrypt.hash(this.password, salt);
-    next();
+    try {
+        if (!this.isModified("password"))        
+            return (next);
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
 
 userSchema.methods.matchPassword = async function (entreredPassword) {
