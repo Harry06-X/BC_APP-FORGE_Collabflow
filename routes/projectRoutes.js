d08c6257ea3e58704ad5d918a18937fa8ec32de5
Harry06-X <harry-hounsou@epitech.eu>
2025-09-22 18:27:49 +0100

    Add the admin authentification on the project route

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { addProject, listProjects, getProjectWithTasks, updateProject, deleteProject } = require("../controllers/projectController");
 const { addTask, listTasksByProject } = require("../controllers/taskController");
+const auth = require("../middleware/auth");
+const { adminOnly } = require("../middleware/roles");
 
-router.post("/", addProject);
+router.post("/", auth, addProject);
 router.get("/", listProjects);
-router.put("/:id", getProjectWithTasks);
+router.put("/:id", auth, getProjectWithTasks);
 router.put("/:id", updateProject);
-router.delete("/:id", deleteProject);
+router.delete("/:id", auth, adminOnly, deleteProject);
 
 router.post("/:projectId/tasks", addTask);
 router.get("/:projectId/tasks", listTasksByProject)
